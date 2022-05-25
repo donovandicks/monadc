075a298c9508126be8e7c8d6d682a2b665c6b7fc
Donovan Dicks <donovan.dicks@outlook.com>
2022-05-24 23:33:13 -0400

    Write main program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-use monadc::parser::parse_program;
+use std::{env, fs};
 
-fn main() {}
+use monadc::{compiler::remove_div_by_1, parser::parse_program, types::Instruction};
+
+fn main() {
+    let mut args: Vec<String> = env::args().collect();
+
+    let content = fs::read_to_string(args.pop().expect("input file path")).unwrap();
+
+    let program = parse_program(content.as_str());
+    analyze(program)
+}
+
+fn get_percent_difference(original_len: usize, optimized_len: usize) -> f64 {
+    let orig_float = f64::from(original_len as i32);
+    let optim_float = f64::from(optimized_len as i32);
+
+    ((orig_float / optim_float) - 1.0) * 100.0
+}
+
+fn analyze(program: Vec<Instruction>) {
+    let original = program.clone();
+    let optimized = remove_div_by_1(program);
+
+    let original_len = original.len();
+    let optimized_len = optimized.len();
+
+    println!(
+        "Original vs. Optimized Length:\t{} vs {}",
+        original_len, optimized_len
+    );
+
+    println!(
+        "Optimization is {:.2}% more efficient.",
+        get_percent_difference(original_len, optimized_len)
+    );
+}
